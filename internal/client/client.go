fc19b86eadd09820533d2e61de53c8ea1b866dc5
Thiago de Paulo <thiago.depaulo@mercadolivre.com>
2021-11-22 13:27:13 -0300

    chore: create types in client

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,5 +1,18 @@
 package client
 
+type Protocol struct {
+	BlockIds []string `json:"block_ids"`
+}
+
+type BlockData struct {
+	BlockId string  `json:"idBloqueio"`
+	Amount  float32 `json:"valor"`
+}
+
+type Block struct {
+	BlockData BlockData `json:"data"`
+}
+
 func GetBlockIds(authToken, protocolId string) ([]string, error) {
 	return []string{"123", "456"}, nil
 }
